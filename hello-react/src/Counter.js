2ce58db00a3977c6e0acea1edaed8a802a1c90e6
Lee-KyuHwun <wxyz0904@naver.com>
2023-09-05 18:25:15 +0900

    feature/4장 마무리

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    //state의 초기값 설정
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
+
+  // constructor(props) {
+  //   super(props);
+  //   //state의 초기값 설정
+  //   this.state = {
+  //     number: 0,
+  //     fixedNumber: 0,
+  //   };
+  // }
 
   render() {
-    const { number } = this.state; // state조회할때는 this.state로 조회한다.
+    const { number, fixedNumber } = this.state; // state조회할때는 this.state로 조회한다.
     return (
       <>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixedNumber}</h2>
+
         <button // onCliick을 통해 버튼이 클릭했을때 호출할 함수를 지정한다.
           onClick={() => {
-            //this. state를 사용하여 state에 새로운 값을 넣을 수 있다.
-            this.setState({ number: number + 1 });
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log('방금 setState가 호출되었다.');
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
