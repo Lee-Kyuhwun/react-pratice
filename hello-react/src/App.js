56c6e152ed8fb81b0517a1d84ded74b00fb7ff9e
Lee-KyuHwun <wxyz0904@naver.com>
2023-09-05 13:15:18 +0900

    점시시간전 한거 업로드

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import MyComponent from './MyComponent';
 import './App.css';
+import Counter from './Counter';
 
 const App = () => {
-  return (
-    <MyComponent name="react" favoriteNumber={1}>
-      리액트
-    </MyComponent>
-  );
+  return <Counter />;
+
+  // <MyComponent name="react" favoriteNumber={1}>
+  //   리액트
+  // </MyComponent>
 };
 
 export default App;
