21fe9fcb34745f9ff06a4afd6bd26085b2083d27
Lee-KyuHwun <wxyz0904@naver.com>
2023-09-06 11:37:53 +0900

    온라인 책 시작

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,14 +5,25 @@ import './App.css';
 import Counter from './Counter';
 import Say from './Say';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
-const App = () => {
-  return <EventPractice />;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)}></ScrollBox>
+        <button onClick={() => this.scrollBox.scrollToBotton()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
   //return <Say />;
   // return <Counter />;
   // <MyComponent name="react" favoriteNumber={1}>
   //   리액트
   // </MyComponent>
-};
+}
 
 export default App;
